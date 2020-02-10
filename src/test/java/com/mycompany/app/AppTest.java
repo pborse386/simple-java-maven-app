2b8b4283d29c6ee1a0fb31b0c51013088fab365b
pborse386 <42861451+pborse386@users.noreply.github.com>
2020-02-10 15:41:18 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,48 +1,56 @@
-package com.mycompany.app;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.After;
-import static org.junit.Assert.*;
+public class ExampleJenkins {
+	
+	private WebDriver driver;		
+	
+	
+	
+	@BeforeClass
+	public void setUp() {
+		System.out.println("*******************");
+		System.out.println("launching IE browser");
+		System.setProperty("webdriver.ie.driver" , "D:\\Exe\\IEDriverServer.exe");
+	    driver = new InternetExplorerDriver();  
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void testPageTitleSampleB() {
+		driver.navigate().to("http://www.google.com");
+		String strPageTitle = driver.getTitle();
+		System.out.println("Page title: - "+strPageTitle);
+		Assert.assertTrue(strPageTitle.equalsIgnoreCase("Google"), "Page title doesn't match");
+	}
+	
+	@Test
+	public void testSampleOne() {
+		System.out.println("Im in test sample one");
+	}
+	
+	@Test
+	public void testSampleTwo() {
+		System.out.println("Im in test sample two");
+	}
+	
+	@Test
+	public void testSampleThree() {
+		System.out.println("Im in test sample three");
+	}
 
-/**
- * Unit test for simple App.
- */
-public class AppTest
-{
-
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @Test
-    public void testAppConstructor() {
-        try {
-            new App();
-        } catch (Exception e) {
-            fail("Construction failed.");
-        }
-    }
-
-    @Test
-    public void testAppMain()
-    {
-        App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
-        }
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
-
-}
+	@AfterClass
+	public void tearDown() {
+		if(driver!=null) {
+			System.out.println("Closing IE browser");
+			driver.quit();
+		}
+	}
+	
